4f8184cf70a7741e6c2c5be0640921c3d8769078
dbs07x <dbs07x@gmail.com>
2022-08-12 22:04:00 -0300

    Continuação da implementação da aplicação

--- Entra21.CSharp.ClinicaVeterinaria/Servico/RacaServico.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Servico/RacaServico.cs
@@ -9,12 +9,27 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Servico
     // ou seja, deverá honrar as clausulas definidas na interface(contrato)
     public class RacaServico : IRacaServico
     {
-        private RacaRepositorio racaRepositorio;
+        private readonly RacaRepositorio _racaRepositorio;
 
         // Construtor: construir o objeto de RacaServico com o mínimo para a correta execução
         public RacaServico(ClinicaVeterinariaContexto contexto)
         {
-            racaRepositorio = new RacaRepositorio(contexto);
+            _racaRepositorio = new RacaRepositorio(contexto);
+        }
+
+        public void Alterar(int id, string nome, string especie)
+        {
+            var raca = new Raca();
+            raca.Id = id;
+            raca.Nome = nome;
+            raca.Especie = especie;
+
+            _racaRepositorio.Atualizar(raca);
+        }
+
+        public void Apagar(int id)
+        {
+            _racaRepositorio.Apagar(id);
         }
 
         public void Cadastrar(string nome, string especie)
@@ -23,9 +38,23 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Servico
             raca.Nome = nome;
             raca.Especie = especie;
 
-            racaRepositorio.Cadastrar(raca);
+            _racaRepositorio.Cadastrar(raca);
 
             Console.WriteLine($"Nome: {nome} espécie: {especie}");
+        }
+
+        public Raca ObterPorId(int id)
+        {
+            var raca = _racaRepositorio.ObterPorId(id);
+
+            return raca;
+        }
+
+        public List<Raca> ObterTodos()
+        {
+            var racasDoBanco = _racaRepositorio.ObterTodos();
+
+            return racasDoBanco;
         }
     }
 }
